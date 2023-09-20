caf194455ca83ad13ca4522cc2ee0faa06269ee4
Cr721432 <danyfalcon3@gmail.com>
2023-09-20 14:55:51 -0600

    Creacion de Class de Articulo, Consola, Producto y Categoria

---
+++ Models/Articulo.cs
@@ -0,0 +1,11 @@
+﻿namespace VideoDa.ClienteBlazor.Models
+{
+    public class Articulo
+    {
+        public int Id { get; set; }
+        public string IdConsola { get; set; }
+        public string Nombre { get; set; }
+        public string Imagen { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
---
+++ Models/Consola.cs
@@ -0,0 +1,13 @@
+﻿namespace VideoDa.ClienteBlazor.Models
+{
+    public class Consola
+    {
+       public int Id { get; set; }
+       public string Nombre { get; set; }
+       public string Descripcion { get; set;}
+       public string Serie { get; set;}
+       public string Imagen { get; set; }
+       public string Fabricante { get; set; }
+       public string AñodeLanzamiento { get; set;}
+    }
+}
