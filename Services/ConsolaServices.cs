620e744f392809d45e40b9e3bd5c6efaff792231
Fernando Diaz <dfer4738@gmail.com>
2023-09-21 22:24:55 +0200

    vista de  consola

--- Services/ConsolaServices.cs
+++ Services/ConsolaServices.cs
@@ -14,9 +14,9 @@ namespace VideoDa.ClienteBlazor.Services
 
         public async Task<IEnumerable<Consola>> GetAll()
         {
-            var optins = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+            var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
             string resp = await _httpCliente.GetStringAsync($"Consola");
-            return JsonSerializer.Deserialize<IEnumerable<Consola>>(resp, optins);
+            return JsonSerializer.Deserialize<IEnumerable<Consola>>(resp, options);
 
 
         }
